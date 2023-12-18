d5ba79340971a6f7525c520114f48648d7f4a79f
Anton Makei <a.makei@softteco.com>
2023-12-18 22:51:02 +0300

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import i18n from './i18n'
-import { I18nextProvider } from 'react-i18next'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import i18n from "./i18n";
+import { I18nextProvider } from "react-i18next";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainPage from "./MainPage.jsx";
+import ListOfRob from "./ListOfRob.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/project123/",
+    element: <App />,
+    children: [
+      {
+        path: "/project123/home",
+        element: <MainPage />,
+      },
+      {
+        path: "/project123/robbers",
+        element: <ListOfRob />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-    <App />
+      <RouterProvider router={router} />
     </I18nextProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
