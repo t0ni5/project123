516a0d55a41b59914352511352dadb95b63f9bee
Anton Makei <a.makei@softteco.com>
2023-12-18 23:18:58 +0300

    change

--- src/Section_divider.jsx
+++ src/Section_divider.jsx
@@ -1,20 +1,16 @@
-import policeLine from "./assets/Police_Line.png";
+import policeLine from './assets/Police_line.png'
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
-function Section_divider() {
-  return (
-    <Col lg={12} md={12} sm={12}>
-      <Image
-        src={policeLine}
-        alt="police line"
-        className="police_line img-fluid"
-      ></Image>
-    </Col>
-  );
+
+
+function Section_divider(){
+ return(
+    <Col lg={12} md={12} sm={12}><Image src={policeLine} alt="police line" className='police_line img-fluid'></Image></Col>
+ )
 }
 
-export default Section_divider;
+export default Section_divider
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MainPage from "./MainPage.jsx";
-import ListOfRob from "./ListOfRob.jsx";
-
 const router = createBrowserRouter([
   {
     path: "/project123/",
@@ -16,10 +13,6 @@ const router = createBrowserRouter([
       {
         path: "/project123/",
         element: <MainPage />,
-      },
-      {
-        path: "/project123/robbers",
-        element: <ListOfRob />,
       },
     ],
   },
